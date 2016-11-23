8802882a95a3629b341217a1a4cb63e3db24c48b
张超 <tombsky@163.com>
2016-11-23 16:09:41 +0800

    'add删除指定文件的脚手架'

---
+++ deletefile.js
@@ -0,0 +1,23 @@
+var fs = require("fs");
+
+/**
+ * 删除指定文件夹中匹配文件
+ */
+console.log("准备删除文件！");
+fs.readdir("locales",function(err, files){
+   if (err) {
+       return console.error(err);
+   }
+   files.forEach( function (file){
+		if(file.lastIndexOf('.min.min.js')>0){
+		
+		fs.unlink('locales/'+file, function(err){
+			if(err){
+				return console.log(err);
+			}else{
+				console.log('删除文件'+file);
+			}
+		});
+		}
+   });
+});
